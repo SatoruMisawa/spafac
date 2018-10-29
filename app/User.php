078a28d7da03194398bcc0ddb00fe864da9e925a
tomocy <tomocy.dev@gmail.com>
2018-10-30 03:47:08 +0900

    remove unused ones

--- app/User.php
+++ app/User.php
@@ -3,7 +3,6 @@
 namespace App;
 
 use App\MyModel;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
