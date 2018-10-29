fab7b40a72400334f1cf00c45d8a44844f8171bb
tomocy <tomocy.dev@gmail.com>
2018-10-30 02:44:53 +0900

    add seeder for default providers

---
+++ database/seeds/ProviderSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Provider;
+use Illuminate\Database\Seeder;
+
+class ProviderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Provider::create([
+            'name' => 'facebook',
+        ]);
+    }
+}
