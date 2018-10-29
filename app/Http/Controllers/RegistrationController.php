46587ce2b162728215c8c53fa41df219a590731d
tomocy <tomocy.dev@gmail.com>
2018-10-29 13:32:39 +0900

    enable to register users

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -31,55 +31,26 @@ class RegistrationController extends FrontController
 	*新規会員登録(確認)
 	*/
 	public function confirm(Request $request) {
-		
-		$user = new User();
-		
-		$user->fillRequestData($request);
-		
-		//バリデート
-		$validateRules = [
+		$request->validate([
+			'nickname' => 'required',
+			'password' => 'required|between:8,20|confirmed',
+			'password_confirmation' => 'required',
 			'name' => 'required',
 			'email' => 'required|email|unique:users,email',
-			'nickname' => 'required',
-			//'profile' => 'required',
-			//'name1' => 'required',
-			//'name2' => 'required',
 			'tel' => 'required|tel',
-			//'industry_id' => '',
-			//'job_id' => '',
-			//'corporation' => '',
 			'zip' => 'required|zip',
 			'prefecture_id' => 'required',
 			'address1' => 'required',
 			'address2' => 'required',
-			'address3' => '',
-			'new_password' => 'required|between:8,20|confirmed',
-			'new_password_confirmation' => 'required',
-		];
-		$validateMessages = [
-		];
-		$validator = \Validator::make($request->all(), $validateRules, $validateMessages);
-		$customAttributes = [
-			'name' => '名前',
-			'email' => 'メールアドレス',
-			'nickname' => 'ニックネーム',
-			'profile' => 'プロフィール',
-		];
-		$validator->addCustomAttributes($customAttributes);
-		if ($validator->fails()) {
-			return redirect()->back()->withErrors($validator)->withInput();
-		}
-		
-		$user->password = $request->input('new_password');
-		$user->email_token = str_random(10);
-		$user->save();
-		
+		]);
+
+		$user = User::create($request->all());
+
 		//メール送信
 		$email = new EmailVerification($user);
 		Mail::to($user->email)->send($email);
 		
 		return redirect()->to('registration/send')->with('message', '確認メールを送信しました。');
-		
 	}
 
 	/**
