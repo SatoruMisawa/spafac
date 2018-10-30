ad55f705a0c0c53682062594fc91c884ab9d6947
tomocy <tomocy.dev@gmail.com>
2018-10-30 21:17:20 +0900

    add addresses migration

---
+++ database/migrations/2018_10_30_211518_create_addresses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('zip');
+            $table->integer('prefecture_id');
+            $table->string('address1');
+            $table->string('address2');
+            $table->string('address3')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
