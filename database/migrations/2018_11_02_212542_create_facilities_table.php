e05bbf366bb7966ab67466b7ce3df5759ae80c2e
tomocy <tomocy.dev@gmail.com>
2018-11-02 22:10:51 +0900

    add Facility

---
+++ database/migrations/2018_11_02_212542_create_facilities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFacilitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facilities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('address_id')->unsigned();
+            $table->bigInteger('facility_kind_id')->unsigned();
+            $table->text('access');
+            $table->string('tel');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facilities');
+    }
+}
