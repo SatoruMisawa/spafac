acad144da4ba27a9e62bd4eabfb3f8a58ec4b440
tomocy <tomocy.dev@gmail.com>
2018-11-02 22:19:58 +0900

    add Schedule

---
+++ database/migrations/2018_11_02_221843_create_schedules_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('plan_id')->unsigned();
+            $table->bigInteger('day_id')->unsigned();
+            $table->time('start');
+            $table->time('end');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
