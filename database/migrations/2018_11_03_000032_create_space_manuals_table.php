c59de4f63f7f2c4cb64ac6d3a981f9f428cd405e
tomocy <tomocy.dev@gmail.com>
2018-11-03 19:08:52 +0900

    fix

--- database/migrations/2018_11_03_000032_create_space_manuals_table.php
+++ database/migrations/2018_11_03_000032_create_space_manuals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSpaceSpaceUsageTable extends Migration
+class CreateSpaceManualsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -27,6 +27,6 @@ class CreateSpaceSpaceUsageTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('space_space_usage');
+        Schema::dropIfExists('space_manuals');
     }
 }
