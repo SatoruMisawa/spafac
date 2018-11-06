00ee7e0607e79da005e16d8d38b719b31458a326
tomocy <tomocy.dev@gmail.com>
2018-11-06 14:32:07 +0900

    fix

--- resources/views/layouts/master_stay.blade.php
+++ resources/views/layouts/master_stay.blade.php
@@ -10,31 +10,26 @@
 </head>
 
 <body class="home page-template-default page page-id-5 logged-in">
-<div id="page" class="site">
-	<!--<a class="skip-link screen-reader-text" href="#content">Skip to content</a>-->
+<div id="page" class="site">	
 	@include('layouts.header_stay')
 	@yield('content')
-		@include('mypage.layouts.footer')
-</div><!-- #page -->
+	@include('layouts.footer')
+</div>
 
 <script type='text/javascript'>
 /* <![CDATA[ */
 var wpcf7 = {"apiSettings":{"root":"https:\/\/space-factory-kinofumi.c9users.io\/wp-json\/contact-form-7\/v1","namespace":"contact-form-7\/v1"},"recaptcha":{"messages":{"empty":"\u3042\u306a\u305f\u304c\u30ed\u30dc\u30c3\u30c8\u3067\u306f\u306a\u3044\u3053\u3068\u3092\u8a3c\u660e\u3057\u3066\u304f\u3060\u3055\u3044\u3002"}}};
 /* ]]> */
 </script>
-<!--
-
-<script type='text/javascript' src='/wp-content/plugins/contact-form-7/includes/js/scripts.js?ver=5.0.1'></script>
+{{-- <script type='text/javascript' src='/wp-content/plugins/contact-form-7/includes/js/scripts.js?ver=5.0.1'></script>
 <script type='text/javascript' src='/wp-content/themes/space-factory/js/navigation.js?ver=20151215'></script>
 <script type='text/javascript' src='/wp-content/themes/space-factory/js/skip-link-focus-fix.js?ver=20151215'></script>
 <script type='text/javascript' src='/wp-content/themes/space-factory/js/select.js?ver=20180213'></script>
 <script type='text/javascript' src='/wp-content/themes/space-factory/js/ofi.min.js?ver=20180215'></script>
 <script type='text/javascript' src='/wp-content/themes/space-factory/js/default.js?ver=20180215'></script>
 <script type='text/javascript' src='/wp-includes/js/wp-embed.min.js?ver=4.9.5'></script>
-<script src="//yubinbango.github.io/yubinbango/yubinbango.js"></script>
--->
+<script src="//yubinbango.github.io/yubinbango/yubinbango.js"></script> --}}
 @yield('script')
-        @include('to-top')
-
+@include('to-top')
 </body>
 </html>
