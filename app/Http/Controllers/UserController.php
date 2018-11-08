c42b0367fa3c3a5ab742a68908289f5b4eea1e11
tomocy <tomocy.dev@gmail.com>
2018-11-08 15:35:01 +0900

    use repository instead

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,19 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Prefecture;
 use App\User;
+use App\Repositories\UserRepository;
 use Auth;
 use App\Http\Requests\CreateUserRequest;
 
 class UserController extends Controller
 {
+    private $repo;
+
+    public function __construct(UserRepository $repo) {
+        $this->repo = $repo;
+    }
+
     public function new() {
-		return view('user.new');
+    return view('user.new');
     }
     
     public function create(CreateUserRequest $request) {		
-		$user = User::create($request->all());
-		Auth::login($user, true);
-				
-		return redirect()->route('verification.email.send', $user->id);
-	}
+    $user = $this->repo->create($request->all());
+    Auth::login($user, true);
+        
+    return redirect()->route('verification.email.send', $user->id);
+  }
 }
