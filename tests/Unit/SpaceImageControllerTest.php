29a6087fd3fd99dce29eddfdb97e328932b27678
tomocy <tomocy.dev@gmail.com>
2018-11-10 19:01:23 +0900

    add test of create in SpaceImageController

--- tests/Unit/SpaceImageControllerTest.php
+++ tests/Unit/SpaceImageControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use App\Space;
 use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -17,5 +18,18 @@ class SpaceImageControllerTest extends TestCase
                         ->get(route('host.space.image.new', $space->id));
         $response->assertStatus(200)
                 ->assertSee('新規スペース画像');
+    }
+
+    public function testCreate() {
+        $this->refreshAndSeedDatabase();
+        $space = factory(Space::class)->create();
+        $response = $this->loginWithTesterIfDebug()
+                        ->loginWithUser()
+                        ->post(route('host.space.image.create', $space->id), [
+                            'images' => [
+                                UploadedFile::fake()->image('test.png'),
+                            ],
+                        ])
+                        ->assertRedirect(route('host.space.plan.new', $space->id));
     }
 }
