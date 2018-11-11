e74537ae79494599ac732c023255d7144f4ecd34
tomocy <tomocy.dev@gmail.com>
2018-11-11 15:41:10 +0900

    fix

--- resources/views/tester/session/new.blade.php
+++ resources/views/tester/session/new.blade.php
@@ -7,7 +7,6 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.5 -->
-  {{--  <link rel="stylesheet" href="https://test.spafac.com/vendor/laravel-admin/AdminLTE/bootstrap/css/bootstrap.min.css"> --}}
   <link rel="stylesheet" href="{{ asset('vendor/laravel-admin/AdminLTE/bootstrap/css/bootstrap.min.css') }}">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{ asset('vendor/laravel-admin/font-awesome/css/font-awesome.min.css') }}">
