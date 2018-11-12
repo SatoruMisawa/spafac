4accfb1c69cf9a7fac61706b4f8aebeea08f57e3
tomocy <tomocy.dev@gmail.com>
2018-11-12 18:58:34 +0900

    add metho to BankAccountController to save a bank account controller

--- app/Http/Controllers/Host/BankAccountController.php
+++ app/Http/Controllers/Host/BankAccountController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers\Host;
 
-use Illuminate\Http\Request;
+use App\Repositories\BankAccountRepository;
+use Auth;
+use App\Http\Requests\CreateBankAccountRequest;
 use App\Http\Controllers\Controller;
 
 class BankAccountController extends Controller
 {
+    private $bankAccountRepository;
+
+    public function __construct(BankAccountRepository $bankAccountRepository) {
+        $this->bankAccountRepository = $bankAccountRepository;
+    }
+
     public function new() {
         return view('host.bankaccount.new');
-    }   
+    }  
+
+    public function create(CreateBankAccountRequest $request) {
+        Auth::user()->bankAccounts()->save(
+            $this->bankAccountRepository->new(
+                $request->only([
+                    'bank_name', 'bank_code',
+                    'branch_name', 'branch_code',
+                    'account_number', 'account_holder',
+                ])
+            )
+        );
+
+        return redirect()->route('host.index');
+    }
 }
--- tests/Unit/BankAccountControllerTest.php
+++ tests/Unit/BankAccountControllerTest.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class BankAccountControllerTest extends TestCase
 {
+    use WithFaker;
     use RefreshDatabase;
 
     public function testNew() {
@@ -17,5 +18,19 @@ class BankAccountControllerTest extends TestCase
 
         $response->assertStatus(200)
                 ->assertSee('新規銀行口座');
+    }
+
+    public function testCreate() {
+        $response = $this->loginWithTesterIfDebug()
+                        ->loginWithUser()
+                        ->post(route('host.bankaccount.create', [
+                            'bank_name' => $this->faker->name(),
+                            'bank_code' => $this->faker->numberBetween(100, 999),
+                            'branch_name' => $this->faker->name(),
+                            'branch_code' => $this->faker->numberBetween(100, 999),
+                            'account_number' => $this->faker->bankAccountNumber(),
+                            'account_holder' => $this->faker->name(),
+                        ]))
+                        ->assertRedirect(route('host.index'));
     }
 }
