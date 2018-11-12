01b6ebdccda32459048b4b01c2346e4db66f774d
tomocy <tomocy.dev@gmail.com>
2018-11-12 18:34:18 +0900

    fix fillable in BankAccount to be able to store user_id

--- app/BankAccount.php
+++ app/BankAccount.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class BankAccount extends Model
 {
     protected $fillable = [
+        'user_id',
         'bank_name', 'bank_code',
         'branch_name', 'branch_code',
         'account_number', 'account_holder',
