356d8e3621df9ef00faa8f15065bb0fb76922a3d
tomocy <tomocy.dev@gmail.com>
2018-11-16 16:18:23 +0900

    add method to MessageTemplate to create a message template

--- app/Http/Controllers/Host/MessageTemplateController.php
+++ app/Http/Controllers/Host/MessageTemplateController.php
@@ -3,16 +3,38 @@
 namespace App\Http\Controllers\Host;
 
 use App\Space;
+use App\Repositories\MessageTemplateRepository;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\CreateMessageTemplateRequest;
 
 class MessageTemplateController extends Controller
 {
+    private $messageTemplateRepository;
+
+    public function __construct(MessageTemplateRepository $messageTemplateRepository) {
+        $this->messageTemplateRepository = $messageTemplateRepository;
+    }
+
     public function new(Space $space) {
         try {
             return view('host.space.messagetemplate.new', compact('space'));
         } catch (\Exception $e) {
             dd($e->getMessage());
         }
+    }
+
+    public function create(CreateMessageTemplateRequest $request, Space $space) {
+        $data = $this->data($request, $space->id);
+        $this->messageTemplateRepository->create($data);
+
+        return redirect()->route('host.space.plan.new', $space->id);
+    }
+
+    private function data(CreateMessageTemplateRequest $request, $spaceID) {
+        return $request->only([
+            'on_apply_approved',
+            'on_apply_rejected',
+            'reminder',
+        ]) + ['space_id' => $spaceID];
     }
 }
--- tests/Feature/MessageTemplateControllerTest.php
+++ tests/Feature/MessageTemplateControllerTest.php
@@ -21,4 +21,35 @@ class MessageTemplateControllerTest extends TestCase
         $response->assertStatus(200)
                  ->assertSee('新規定型文');
     }
+
+    public function testCreate() {
+        $space = factory(Space::class)->create();
+        $data = $this->data();
+        $this->assertPostRequestToCreateMessageTemplate($space->id, $data);
+        $this->assertMessageTemplateInDB($space->id, $data);
+    }
+
+    private function data() {
+        return [
+            'on_apply_approved' => $this->faker->sentence(),
+            'on_apply_rejected' => $this->faker->sentence(),
+            'reminder' => $this->faker->sentence(),
+        ];
+    }
+
+    private function assertPostRequestToCreateMessageTemplate($spaceID, $data) {
+        return $this->loginWithTesterIfDebug()
+                    ->loginWithUser()
+                    ->post(route('host.space.messagetemplate.create', $spaceID), $data)
+                    ->assertRedirect(route('host.space.plan.new', $spaceID));
+    }
+
+    private function assertMessageTemplateInDB($spaceID, $data) {
+        $this->assertDatabaseHas('message_templates', [
+            'space_id' => $spaceID,
+            'on_apply_approved' => $data['on_apply_approved'],
+            'on_apply_rejected' => $data['on_apply_rejected'],
+            'reminder' => $data['reminder'],
+        ]);
+    }
 }
