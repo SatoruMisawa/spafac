e44fc4f5e796545feacd676f50ab3300fe6f4ec6
tomocy <tomocy.dev@gmail.com>
2018-11-16 15:54:34 +0900

    add MessageTemplateRepository

---
+++ app/Repositories/MessageTemplateRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface MessageTemplateRepository.
+ *
+ * @package namespace App\Repositories;
+ */
+interface MessageTemplateRepository extends RepositoryInterface
+{
+    //
+}
