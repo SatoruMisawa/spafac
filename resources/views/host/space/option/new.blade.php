c22b73b7a7f53ebd46a115db633f5fe7594d2cc9
tomocy <tomocy.dev@gmail.com>
2018-11-18 13:51:07 +0900

    add view for option

---
+++ resources/views/host/space/option/new.blade.php
@@ -0,0 +1,83 @@
+@extends('host.layouts.master')
+@section('content')
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        新規オプション
+        <small></small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="{{ url('host') }}"><i class="fa fa-dashboard"></i> ホーム</a></li>
+    </ol>
+</section>
+<!-- Main content -->
+<section class="content container-fluid">
+    @include('host.layouts.message', ['errors' => $errors])
+    <div class="row">
+        <div class="col-md-12">
+            {{
+            Form::open([
+            'route' => ['host.space.option.create', $space->id],
+            'method' => 'POST',
+            ])
+            }}
+            <div class="box box-info">
+                <div class="box-body pad">
+                    <div class="form-group {{ App\Helper::errorClass($errors, ['options[]']) }}">
+                        <label><small class="label bg-blue">任意</small> オプション</label>
+                        @include('layouts.error', ['name' => 'options[]'])
+                        <div class="row">
+                            <label>名前</label>
+                            <div class="col-sm-9">
+                                {{
+                                Form::text(
+                                'options[][name]',
+                                null,
+                                [
+                                'class' => 'form-control',
+                                ]
+                                )
+                                }}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <label>値段</label>
+                            <div class="col-sm-9">
+                                {{
+                                Form::text(
+                                'options[][price]',
+                                null,
+                                [
+                                'class' => 'form-control',
+                                ]
+                                )
+                                }}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <label>回数制限</label>
+                            <div class="col-sm-9">
+                                {{
+                                Form::text(
+                                'options[][limit]',
+                                null,
+                                [
+                                'class' => 'form-control',
+                                ]
+                                )
+                                }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="box-footer">
+                    <button type="submit" class="btn btn-default" disabled>保存して戻る</button>
+                    <button type="submit" class="btn btn-success pull-right">保存して進む</button>
+                </div>
+            </div>
+            {{ Form::close() }}
+        </div>
+    </div>
+</section>
+<!-- /.content -->
+@endsection
