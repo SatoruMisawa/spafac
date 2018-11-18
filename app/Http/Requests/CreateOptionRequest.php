b9b93feef9d92d0308b893f2b87fba821f24f027
tomocy <tomocy.dev@gmail.com>
2018-11-18 13:35:01 +0900

    add CreateOptionRequest

---
+++ app/Http/Requests/CreateOptionRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateOptionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'options' => 'nullable|array',
+            'options.name' => 'nullable|string',
+            'options.price' => 'nullable|required_with:name|integer',
+            'options' =>'nullable|integer',
+        ];
+    }
+}
