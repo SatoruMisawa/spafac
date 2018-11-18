7a79fd375c4288cf0cb35b41715acbb21c14a7ac
tomocy <tomocy.dev@gmail.com>
2018-11-18 14:51:16 +0900

    add OptionController for option

---
+++ app/Http/Controllers/Host/OptionController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Host;
+
+use App\Space;
+use App\Repositories\OptionRepository;
+use App\Http\Requests\CreateOptionRequest;
+use App\Http\Controllers\Controller;
+
+class OptionController extends Controller
+{
+    private $optionRepository;
+
+    public function __construct(OptionRepository $optionRepository) {
+        $this->optionRepository = $optionRepository;
+    }
+
+    public function new(Space $space) {
+        return view('host.space.option.new', compact('space'));
+    }
+
+    public function create(CreateOptionRequest $request, Space $space) {
+        foreach ($request->get('options') as $option) {
+            if (!isset($option['name'])) {
+                continue;
+            }
+            $this->optionRepository->create([
+                'space_id' => $space->id,
+                'name' => $option['name'],
+                'price' => $option['price'],
+                'limit' => $option['limit'],
+            ]);
+        }
+
+        return redirect()->route('host.space.plan.new', $space->id);
+    }
+}
---
+++ tests/Feature/OptionControllerTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Space;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class OptionControllerTest extends TestCase
+{
+    use WithFaker;
+    use RefreshDatabase;
+
+    public function testNew() {
+        $space = factory(Space::class)->create();
+        $response = $this->loginWithTesterIfDebug()
+                        ->loginWithUser()
+                        ->get(route('host.space.option.new', $space->id));
+        $response->assertStatus(200)
+                ->assertSee('新規オプション');
+    }
+
+    public function testCreate() {
+        try {
+            $space = factory(Space::class)->create();
+        $data = $this->data();
+        $this->assertPostRequestToCreateOptions($space->id, $data);
+        $this->assertOptionsInDB($space->id, $data);
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+
+    private function data() {
+        return [
+            'options' => [
+                [
+                    'name' => $this->faker->name(),
+                    'price' => $this->faker->numberBetween(1, 9999),
+                    'limit' => $this->faker->numberBetween(1, 99),
+                ],
+                [
+                    'name' => $this->faker->name(),
+                    'price' => $this->faker->numberBetween(1, 9999),
+                    'limit' => null,
+                ],
+            ],
+        ];
+    }
+
+    private function assertPostRequestToCreateOptions($spaceID, $data) {
+        return $this->loginWithTesterIfDebug()
+                    ->loginWithUser()
+                    ->post(route('host.space.option.create', $spaceID), $data)
+                    ->assertRedirect(route('host.space.plan.new', $spaceID));
+    }
+
+    private function assertOptionsInDB($spaceID, $data) {
+        foreach ($data['options'] as $option) {
+            $this->assertDatabaseHas('options', [
+                'space_id' => $spaceID,
+                'name' => $option['name'],
+                'price' => $option['price'],
+                'limit' => $option['limit'],
+            ]);
+        }
+    }
+}
