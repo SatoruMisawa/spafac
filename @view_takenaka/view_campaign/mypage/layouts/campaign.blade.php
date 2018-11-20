eb28a64d025df45629c871c48326d868a9fe12ae
spafac <developer@spafac.com>
2018-11-20 17:12:45 +0900

    addition

---
+++ @view_takenaka/view_campaign/mypage/layouts/campaign.blade.php
@@ -0,0 +1,7 @@
+<div class="campaign">
+	<div class="campaign_date"><span>12月末まで</span></div>
+	<div class="campaign_hashtag">
+		<p>12月末まで弊社スペース登録代行　無料キャンペーン実施中！<br>お持ちのスペースを有効活用して収益化UP!!</p>
+	</div>
+	<div class="campaign_detail_btn"><a href="http://spafac.co.jp/">キャンペーン詳細へ</a></div>
+</div>
