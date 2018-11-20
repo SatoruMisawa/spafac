85ec98100606ee779f6524226fd2d84110ed44f7
tomocy <tomocy.dev@gmail.com>
2018-11-20 18:59:07 +0900

    add ReservationFactory

--- database/factories/ReservationFactory.php
+++ database/factories/ReservationFactory.php
@@ -6,9 +6,17 @@ use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Reservation::class, function (Faker $faker) {
+    $host = factory(User::class)->create();
+    $host->claimantUser()->create([
+        'claimant_account_id' => str_random(30),
+    ]);
+    $guest = factory(User::class)->create();
+    $guest->claimantUser()->create([
+        'claimant_source_id' => str_random(30),
+    ]);
     return [
-        'host_id' => factory(User::class)->create()->id,
-        'guest_id' => factory(User::class)->create()->id,
+        'host_id' => $host->id,
+        'guest_id' => $guest->id,
         'apply_id' => factory(Apply::class)->create()->id,
     ];
 });
