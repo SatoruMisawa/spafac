43a41ec72bfb38c53eeb9dd3306c575f05cada6b
tomocy <tomocy.dev@gmail.com>
2018-11-20 17:03:38 +0900

    fix Reservation

--- app/Reservation.php
+++ app/Reservation.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     protected $fillable = [
-        'user_id', 'apply_id',
+        'user_id', 'apply_id', 'is_charged',
     ];
 
     public function user() {
--- database/migrations/2018_10_30_212203_create_reservations_table.php
+++ database/migrations/2018_10_30_212203_create_reservations_table.php
@@ -17,6 +17,7 @@ class CreateReservationsTable extends Migration
             $table->increments('id');
             $table->bigInteger('user_id')->unsigned();
             $table->bigInteger('apply_id')->unsigned();
+            $table->boolean('is_charged')->default(false);
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
