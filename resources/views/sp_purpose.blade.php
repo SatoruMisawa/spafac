c809becfcab219d9d324f4df4d29c01c7f663f4c
spafac <developer@spafac.com>
2018-11-23 15:42:06 +0900

    addition

---
+++ resources/views/sp_purpose.blade.php
@@ -0,0 +1,18 @@
+<div class="party_search_list bgcg">
+    <h2>目的で探す</h2>
+        <div class="wrap party_abt">
+            <p>
+                <a href="/purpose/sales">物販</a>
+                <a href="/purpose/party">飲食・パーティー</a>
+                <a href="/purpose/office">オフィス・会議</a>
+                <a href="/purpose/event">イベントプロモーション・広告</a>
+                <a href="/purpose/exhibitionhall">催事・展示会</a>
+                <a href="/purpose/performance">演奏</a>
+                <a href="/stay">宿泊・民泊</a>
+                <a href="/purpose/location">ロケ撮影･写真･動画</a>
+                <a href="/purpose/wedding">結婚式・お祝いシーン</a>
+                <a href="/purpose/parking">駐車場</a>
+                <a href="/purpose/sports">スポーツ</a>
+                <a href="/purpose/other">その他</a>
+            </p>
+</div>
