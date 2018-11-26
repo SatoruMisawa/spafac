8f1ae835b0f0d59d9056b483fa17fc15492a84cd
tomocy <tomocy.dev@gmail.com>
2018-11-26 18:29:40 +0900

    add ApplyRepository

---
+++ app/Repositories/ApplyRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface ApplyRepository.
+ *
+ * @package namespace App\Repositories;
+ */
+interface ApplyRepository extends RepositoryInterface
+{
+    //
+}
