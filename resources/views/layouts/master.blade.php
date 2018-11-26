68536dcecbd193292f05b5db56c55c33b451cfa9
spafac <developer@spafac.com>
2018-11-26 16:18:43 +0900

    amendment

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,8 +5,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>スペースファクトリー</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel = "stylesheet" type = "text/css " href = "{{ asset('assets/css/import.css') }}" >
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <link rel="stylesheet" href="{{ asset('assets/css/search.css') }}">
+<link href = "https://use.fontawesome.com/releases/v5.0.6/css/all.css " rel = " stylesheet " >
 <script src="<?php echo url('assets/common/js/common.js'); ?>"></script>
 </head>
 
